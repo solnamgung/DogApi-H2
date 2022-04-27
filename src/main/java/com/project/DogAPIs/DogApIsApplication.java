e3c33a0c403da94685397650584aa57071b4f256
sol namgung <nandaras0103@gmail.com>
2022-04-27 22:31:10 +0900

    first commit

---
+++ src/main/java/com/project/DogAPIs/DogApIsApplication.java
@@ -0,0 +1,13 @@
+package com.project.DogAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/DogAPIs/DogApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.DogAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
