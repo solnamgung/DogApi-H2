eebdd8b757b7f41a878efcf9cd2b65e2782efb67
sol namgung <nandaras0103@gmail.com>
2022-04-28 10:04:33 +0900

    fixed repository

--- src/main/java/com/project/DogAPIs/repository/DogRepository.java
+++ src/main/java/com/project/DogAPIs/repository/DogRepository.java
@@ -9,12 +9,12 @@ import java.util.List;
 public interface DogRepository extends CrudRepository<Dog, Long> {
     //The @Query annotation declares finder queries directly on repository methods.
     //When I use an id as input to findBreedById, I needed to add a : into the query line to feed in from my method.
-    @Query("select d.id, d.breed from Dog d where d.id=:id")
+    @Query("select id, breed from Dog  where id=:id")
     String findBreedById(Long id);
 
-    @Query("select d.id, d.breed from Dog d")
+    @Query("select id, breed from Dog  ")
     List<String> findAllBreed();
 
-    @Query("select d.id, d.name from Dog d")
+    @Query("select id, name from Dog ")
     List<String> findAllName();
 }
